4691f0befbf17c9635cab450669ea5731ef59bad
Joaquín <dominguezbustosjoaquin@gmail.com>
2023-12-06 20:22:35 -0300

    ALL

---
+++ src/client/java/modingsummer/ModingSummerClient.java
@@ -0,0 +1,10 @@
+package modingsummer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModingSummerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
